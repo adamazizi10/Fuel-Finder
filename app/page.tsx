36b05b309838ef7b6373f861904b134a5c7b6ed8
Adam Azizi <adamazizi101@gmail.com>
2024-02-13 15:31:32 -0500

    deployment

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default async function Home() {
       <main className="mx-auto mt-10 max-w-6xl px-4">
         <NearbyGasStations />
         <div className='mt-20'>
-          <h2 className='mt-8 pb-8 text-4xl font-bold text-white'>Toronto Gas Stations</h2>
+          <h2 className='mt-8 pb-8 text-4xl font-bold text-white'>Toronto Gas Stations (Default)</h2>
           <div className='grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-6'>
             {gasStations.map((gasStation: GasStationType, idx: number) => (
               <Card
